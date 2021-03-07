63a0220d626b49af9dc2cae883603d5ae9c7fdb8
SachaBogaers <san_bogaers@hotmail.com>
2021-03-07 18:19:21 +0100

    Added filter functionality

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-console.log(movies);
 const movielist = document.querySelector("#movie-list");
-console.log(movielist);
+
+const getImdbUrl = id => `https://www.imdb.com/title/${id}`
 
 const addMoviesToDom = movies => {
 	movies.forEach(movie => {
-		console.log(movie);
 		const newMovie = document.createElement("li");
+		const newMovieURL = document.createElement("a");
+		newMovieURL.href = getImdbUrl(movie.imdbID);
+		newMovieURL.target = "_blank"
+		newMovie.appendChild(newMovieURL);
 		const newMoviePoster = document.createElement("img");
 		newMoviePoster.src = movie.Poster;
-		newMoviePoster.className = "movie-poster"
-		newMovie.appendChild(newMoviePoster);
+		newMoviePoster.className = "movie-poster";
+		newMovieURL.appendChild(newMoviePoster);
 		movielist.appendChild(newMovie);
 	})
 }
 addMoviesToDom(movies);
+
+
+const radioButtons = document.getElementsByName("filter-button");
+
+const filterMovies = (value) => {
+	let filteredMovies = movies;
+	switch (value) {
+		case "all":
+			break;
+		case "latest":
+			const filterLatestMovies = movie => {
+				return movie.Year >= 2004;
+			}
+			filteredMovies = movies.filter(filterLatestMovies);
+			break;
+		case "avengers": case "x-men": case "princess": case "batman":
+			const includedInTitle = movie => {
+				const movieTitle = movie.Title.toLowerCase();
+				return movieTitle.includes(value);
+			}
+			filteredMovies = movies.filter(includedInTitle)
+			break;
+		default:
+			break;
+	}
+	movielist.innerHTML = "";
+	addMoviesToDom(filteredMovies);
+}
+
+radioButtons.forEach(button => {
+	button.addEventListener("change", () => {
+		filterMovies(button.value);
+	})
+});
